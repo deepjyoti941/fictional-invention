c01fdb696fbb8d1f1b9303e795372cc346b7d49d
Deepjyoti Khakhlary <deepjyoti941@gmail.com>
2018-09-13 23:30:47 +0530

    create requireAuth Higher Order Function

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-export default () => (
-  <div>
-    <CommentBox />
-    <CommentList />
-  </div>
-);
+class App extends Component {
+  renderButton() {
+    const { auth, changeAuth } = this.props;
+    if (auth) {
+      return (
+        <button type="button" onClick={() => changeAuth(false)}>
+          Sign Out
+        </button>
+      );
+    }
+    return (
+      <button type="button" onClick={() => changeAuth(true)}>
+        Sign In
+      </button>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
