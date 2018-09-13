2dac55df723fcfb6ff700329537b305567afbfd0
Deepjyoti Khakhlary <deepjyoti941@gmail.com>
2018-09-14 02:40:30 +0530

    Creating first async middleware

---
+++ src/middlewares/async.js
@@ -0,0 +1,29 @@
+/**
+ * middleware syntax
+ */
+// export default function({dispatch}) {
+//   return function(next) {
+//     return function(action) {
+
+//     }
+//   }
+// }
+
+export default ({ dispatch }) => next => (action) => {
+  // check to see if the action
+  // has promise on its 'payload' property
+  // If it does, then wait for it to resolve
+  // If it doesn't, then send action on to the next middleware
+
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  // we want to wait for the promise to resolve
+  // (get its data!!) and then create a new action
+  // with that data and dispatch it
+  action.payload.then((response) => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+};
