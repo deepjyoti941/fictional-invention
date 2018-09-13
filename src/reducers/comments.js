fbdb4d3da81391962cb77cbdd2556cfe821fd99a
Deepjyoti Khakhlary <deepjyoti941@gmail.com>
2018-09-14 03:12:09 +0530

    stateValidator middleware with tv4 & jsonschema

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -4,7 +4,7 @@ export default function (state = [], action) {
   let comments;
   switch (action.type) {
   case SAVE_COMMENT:
-    return [...state, action.payload];
+    return [...state, action.payload, {}];
   case FETCH_COMMENTS:
     comments = action.payload.data.map(comment => comment.name);
     return [...state, ...comments];
