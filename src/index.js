e3602cad0e1d0fd5cbe1cc0849f49f5db25d26dd
Deepjyoti Khakhlary <deepjyoti941@gmail.com>
2018-09-27 23:45:19 +0530

    dynamic component loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Loadable from 'react-loadable';
 import Root from 'Root';
 
-import App from 'components/App';
+// import App from 'components/App';
+const LoadableComponent = Loadable({
+  loader: () => import('components/App'),
+  loading() {
+    return <div>Loading...</div>;
+  },
+  delay: 900
+});
 
 ReactDOM.render(
   <Root>
-    <App />
+    <LoadableComponent />
   </Root>,
   document.querySelector('#root')
 );
